1cc77327ad8084eb4d0e4e95b8874f1af69da59b
DESKTOP-L9JE22P\98tg <kdw98tg@gmail.com>
2023-08-31 23:42:51 +0900

    Machine Time 넣는데 성공!

--- RawDataCollector/app/src/main/java/com/bst/rawdatacollector/MainActivity/Main_Worker/ProductInfo/Machine/MachineInfoFragment.kt
+++ RawDataCollector/app/src/main/java/com/bst/rawdatacollector/MainActivity/Main_Worker/ProductInfo/Machine/MachineInfoFragment.kt
@@ -1,14 +1,19 @@
 package com.bst.rawdatacollector.MainActivity.Main_Worker.ProductInfo.Machine
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.TimePicker
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.bst.rawdatacollector.R
 import com.bst.rawdatacollector.SpinnerInterface.CustomSpinnerAdapter
 import com.bst.rawdatacollector.databinding.FragmentMachineInfoBinding
 import okhttp3.Call
@@ -24,30 +29,83 @@ import java.io.IOException
 
 class MachineInfoFragment : Fragment()
 {
-    private lateinit var binding:FragmentMachineInfoBinding
-    private lateinit var errorType:ArrayList<String>
+    private lateinit var binding: FragmentMachineInfoBinding
+    private lateinit var errorType: ArrayList<String>
 
-    companion object{
-        private const val SELECT_ERROR_TYPE_URL ="http://kdw98tg.dothome.co.kr/RDC/Select_ErrorType.php/"
+    companion object
+    {
+        private const val SELECT_ERROR_TYPE_URL = "http://kdw98tg.dothome.co.kr/RDC/Select_ErrorType.php/"
     }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
     {
         binding = FragmentMachineInfoBinding.inflate(layoutInflater)
-        //init
-        errorType = ArrayList()
-
-        selectErrorType("설비")
-
         return binding.root
     }
 
-    private fun selectErrorType(_type:String)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        //init
+        errorType = ArrayList()
+        selectErrorType("설비")
+
+        binding.startTimeBtn.setOnClickListener {
+            showMachineErrorTimeDialog(binding.startTimeBtn)
+        }
+        binding.endTimeBtn.setOnClickListener {
+            showMachineErrorTimeDialog(binding.endTimeBtn)
+        }
+
+    }
+
+    private fun showMachineErrorTimeDialog(btn: Button)
+    {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        val inflater = LayoutInflater.from(requireContext())
+
+        //dialogView Init
+        val dialogView = inflater.inflate(R.layout.dialog_machine_error_time, null)
+        val timePicker: TimePicker = dialogView.findViewById(R.id.timePicker)
+
+        dialogBuilder.setView(dialogView)
+
+        timePicker.setOnTimeChangedListener(object : TimePicker.OnTimeChangedListener
+        {
+            @SuppressLint("SetTextI18n")
+            override fun onTimeChanged(_timePicker: TimePicker?, _hour: Int, _minute: Int)
+            {
+                if (_hour > 12)
+                {
+                    btn.text = "오후 $_hour : $_minute"
+                }
+                else
+                {
+                    btn.text = "오전 $_hour : $_minute"
+                }
+            }
+        })
+
+        dialogBuilder.setNegativeButton("취소") { dialog, which ->
+            // 취소 버튼 클릭 시 아무 작업도 수행하지 않음
+            dialog.dismiss()
+        }.setPositiveButton("확인") { dialog, which ->
+            Toast.makeText(requireContext(), "저장 되었습니다.", Toast.LENGTH_SHORT).show()
+
+
+            //통신 해야 함
+        }
+        val alertDialog = dialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun selectErrorType(_type: String)
     {
         val client = OkHttpClient()
-        val body = FormBody.Builder().add("type",_type).build()
+        val body = FormBody.Builder().add("type", _type).build()
         val request = Request.Builder().url(SELECT_ERROR_TYPE_URL).post(body).build()
 
-        client.newCall(request).enqueue(object: Callback
+        client.newCall(request).enqueue(object : Callback
         {
             override fun onFailure(call: Call, e: IOException)
             {
@@ -56,7 +114,7 @@ class MachineInfoFragment : Fragment()
 
             override fun onResponse(call: Call, response: Response)
             {
-                if(response.isSuccessful)
+                if (response.isSuccessful)
                 {
                     activity?.runOnUiThread {
                         try
@@ -70,8 +128,7 @@ class MachineInfoFragment : Fragment()
                                 val errorName = json.getString("error_name")
                                 errorType.add(errorName)
 
-                                setSpinnerAdapter(binding.workSettingSpinner,requireContext(), errorType)
-                                setSpinnerAdapter(binding.machineErrorSpinner,requireContext(), errorType)
+                                setSpinnerAdapter(binding.workSettingSpinner, requireContext(), errorType)
                             }
                         }
                         catch (e: Exception)
@@ -86,7 +143,7 @@ class MachineInfoFragment : Fragment()
 
     private fun setSpinnerAdapter(spinner: Spinner, context: Context, stringLists: ArrayList<String>)
     {
-        val spinnerAdapter: CustomSpinnerAdapter = CustomSpinnerAdapter(context, stringLists)
+        val spinnerAdapter = CustomSpinnerAdapter(context, stringLists)
         spinner.adapter = spinnerAdapter//어뎁터 생성
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener
         {
@@ -103,8 +160,6 @@ class MachineInfoFragment : Fragment()
             }
 
         }
-
     }
-
 
 }
